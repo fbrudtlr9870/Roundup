918aedbbaa12789f27c521f234ab0fc76c116fe4
jackalpsh <tkdgus2918@naver.com>
2018-06-19 13:20:45 +0900

    분류별 검색

--- Roundup/src/main/java/com/proj/rup/product/model/service/ProductService.java
+++ Roundup/src/main/java/com/proj/rup/product/model/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.proj.rup.product.model.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.proj.rup.product.model.vo.Product;
 
 public interface ProductService {
 
 	List<Product> productSearch(String searchKeyword);
+
+	List<Product> reSearch(Map<String, Object> map);
 
 }
