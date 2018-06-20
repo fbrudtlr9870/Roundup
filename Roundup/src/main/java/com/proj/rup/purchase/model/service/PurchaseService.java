98c49134902d3405588ea057cb226ef8588e59d2
hyelin0111 <sjgpfls@gmail.com>
2018-06-20 15:25:45 +0900

    hyelin
    basket, purchase

--- Roundup/src/main/java/com/proj/rup/purchase/model/service/PurchaseService.java
+++ Roundup/src/main/java/com/proj/rup/purchase/model/service/PurchaseService.java
@@ -2,11 +2,13 @@ package com.proj.rup.purchase.model.service;
 
 import java.util.List;
 
+import com.proj.rup.basket.model.vo.BasketProduct;
 import com.proj.rup.purchase.model.vo.PurchaseComplete;
 
 public interface PurchaseService {
 
 	List<PurchaseComplete> selectPCList(String memberId);
 
+	BasketProduct selectPurchaseOne(int basketNo);
 
 }
